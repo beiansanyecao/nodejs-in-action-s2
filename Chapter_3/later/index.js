fda198f76e64d925a5e8dcb67b75fa88599e8b0c
岳建南 <jiannan.jyn@alibaba-inc.com>
2018-11-29 18:56:34 +0800

    daily progress

--- Chapter_3/later/index.js
+++ Chapter_3/later/index.js
@@ -1,15 +1,77 @@
 // index.js
 
 const express = require('express');
-
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const Article = require('./db').Article;
+const read = require('node-readability');
+const JSON = require('circular-json');
 const app = express();
-
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello Express!');
-})
+app.set('port', port);
 
-app.listen(port, () => {
-    console.log(`Express web app available at localhost: ${port}`);
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/articles', (req, res, next) => {
+
+    Article.all((err, articles) => {
+        if (err) return next(err);
+        res.send(articles);
+    })
+
+});
+
+app.get('/articles/:id', (req, res, next) => {
+
+    const id = req.params.id;
+
+    Article.find(id, (err, article) => {
+        if (err) return next(err);
+        console.log(article);
+        res.send(article);        
+    })
+    
+});
+
+app.post('/articles', (req, res, next) => {
+   
+    const url = req.body.url;
+
+    read(url, (err, result) => {
+
+        if (err || !result) 
+            res.status(500).send('Error downloading article');
+
+
+        Article.create(
+          { title: result.title, content: result.content },
+          (err, article) => {
+            if (err) return next(err);
+            console.log(article);
+            res.send('OK');
+
+            console.log('OK');
+          }
+        );
+
+    });
+
+});
+
+app.delete('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+
+    Article.delete(id, (err) => {
+        if (err) return err;
+        res.send({ message: 'Deleted' });
+    })
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`App started on port: ${app.get('port')}`);
+});
+
+module.exports = app;
+
