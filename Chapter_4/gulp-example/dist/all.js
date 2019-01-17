1a1bfc453e84c505f2ba5bce3d661afa6138f343
岳建南 <beiansanyecao@gmail.com>
2019-01-17 17:50:08 +0800

    daily progress gulp

---
+++ Chapter_4/gulp-example/dist/all.js
@@ -0,0 +1,2 @@
+"use strict";var _react=require("react"),_react2=_interopRequireDefault(_react),_reactDom=require("react-dom"),_reactDom2=_interopRequireDefault(_reactDom);function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}_reactDom2.default.render(_react2.default.createElement("h1",null,"Hello, world1!"),document.getElementById("example"));
+//# sourceMappingURL=all.js.map
--- Chapter_4/gulp-example/gulpfile.js
+++ Chapter_4/gulp-example/gulpfile.js
@@ -1,19 +1,27 @@
 const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
+const react = require('gulp-react');
 const concat = require('gulp-concat');
-const watch = require('gulp-watch');
+const uglify = require('gulp-uglify');
+const watch = require('gulp-watch'); 
+
+// ES6转ES5时，使用@babel/preset-env，并在.babelrc中指定presets为"env"
+// React .jsx文件转译时，使用babel插件
 
 gulp.task('default', () => {
-
     return gulp.src('app/*.jsx')
-            .pipe(sourcemaps.init())
-            .pipe(babel())
-            .pipe(concat('all.js'))
-            .pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest('dist'));
-})
+        // .pipe(babel())
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            plugins: ['transform-react-jsx']
+        }))
+        .pipe(uglify())
+        .pipe(concat('all.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'));
+}); 
 
-gulp.task('watch', () => {
-    watch('app/**.jsx', () => gulp.start('default'));
-});
+// gulp.task('watch', () => {
+//     watch('app/*.jsx', () => gulp.start('default'));
+// }); 
