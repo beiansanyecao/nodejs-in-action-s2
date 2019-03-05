930c2654440cc9c83fe3b90e714362a20929dbd6
岳建南 <beiansanyecao@gmail.com>
2019-03-05 14:45:20 +0800

    local update

--- Chapter_3/later/index.js
+++ Chapter_3/later/index.js
@@ -1,90 +1,68 @@
-// index.js
-
 const express = require('express');
-const fs = require('fs');
 const bodyParser = require('body-parser');
+const app = express();
 const Article = require('./db').Article;
 const read = require('node-readability');
-const JSON = require('circular-json');
-const app = express();
-const port = process.env.PORT || 3000;
 
-app.set('port', port);
+app.set('port', process.env.PORT || 3000);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-fs.exists('node_modules/bootstrap/dist/css/bootstrap.css', (exists) => {
-    console.log(exists);
-})
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
-    '/css/bootstrap.css',
-    express.static('node_modules/bootstrap/dist/css/bootstrap.css')
-  );
+  '/css/bootstrap.css',
+  express.static('node_modules/bootstrap/dist/css/bootstrap.css')
+);
 
 app.get('/articles', (req, res, next) => {
+  Article.all((err, articles) => {
+    if (err) return next(err);
 
-    Article.all((err, articles) => {
-        if (err) return next(err);
-        
-        res.format({
-            html: () => {
-                res.render('articles.ejs', { articles })
-            },
-            json: () => {
-                res.send(articles);
-            }
-        })
-    })
-
+    res.format({
+      html: () => {
+        res.render('articles.ejs', { articles });
+      },
+      json: () => {
+        res.send(articles);
+      }
+    });
+  });
 });
 
 app.get('/articles/:id', (req, res, next) => {
-
-    const id = req.params.id;
-
-    Article.find(id, (err, article) => {
-        if (err) return next(err);
-        res.send(article);        
-    })
-    
-});
-
-app.post('/articles', (req, res, next) => {
-   
-    const url = req.body.url;
-
-    read(url, (err, result) => {
-
-        if (err || !result) 
-            res.status(500).send('Error downloading article');
-
-        Article.create(
-          { title: result.title, content: result.content },
-          (err, article) => {
-            if (err) return next(err);
-            res.send('OK');
-          }
-        );
-
-    });
-
+  const id = req.params.id;
+  Article.find(id, (err, article) => {
+    if (err) return next(err);
+    res.send(article);
+  });
 });
 
 app.delete('/articles/:id', (req, res, next) => {
-    const id = req.params.id;
+  const id = req.params.id;
+  Article.delete(id, (err) => {
+    if (err) return next(err);
+    res.send({ message: 'Deleted' });
+  });
+});
 
-    Article.delete(id, (err) => {
-        if (err) return err;
-        res.send({ message: 'Deleted' });
-    })
+app.post('/articles', (req, res, next) => {
+  const url = req.body.url;
 
+  read(url, (err, result) => {
+    if (err || !result) res.status(500).send('Error downloading article');
+    Article.create(
+      { title: result.title, content: result.content },
+      (err, article) => {
+        if (err) return next(err);
+        console.log(article);
+        res.send('OK');
+      }
+    );
+  });
 });
 
 app.listen(app.get('port'), () => {
-    console.log(`App started on port: ${app.get('port')}`);
+  console.log('App started on port', app.get('port'));
 });
 
 module.exports = app;
-
